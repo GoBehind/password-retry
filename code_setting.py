4fbc36a1c0f6a9813c1d49c0fb773de54d3fbfb9
KuanChih Wang <n789142536@hotmail.com>
2019-03-23 10:57:59 +0800

    First Commit

---
+++ code_setting.py
@@ -0,0 +1,17 @@
+password = 'a123456'
+time = 3
+print('最多輸入3次密碼')
+
+while True:
+	guess = input('請輸入密碼: ', )
+
+	if guess == password:
+		print('登入成功')
+		break
+	else:
+		time = time - 1
+		print('密碼錯誤, ''您還剩', time, '次機會')
+
+		if time <= 0:
+			print('輸入次數已達上限')
+			break
